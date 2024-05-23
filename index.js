373566afede20ffc628f8b15a22316cb6ca42c9a
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-05-23 19:29:51 +0530

    backend start

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log('namaste bharat');
+// console.log("jai hind, bande matram");
+
+const http = require("http");
+
+// console.log(http);
+
+// to make server we'll do following things
+
+http.createServer(() => {});
+
+// Server.listen(5000,()=>{
+
+// })
+
+server.listen(5000, () => {
+  console.log("server is working");
+});

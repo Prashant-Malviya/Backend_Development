1e38f3dd37c2b7024db007113b21d11730d02db1
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-05-28 19:27:46 +0530

    ejs basic completed

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import express from 'express';
-
+import path from 'path'
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    // res.send("hi");
-    // res.sendStatus(500);
-    // res.sendStatus(404)// these are known as status code
+// app.get("/",(req,res)=>{
+//     // res.send("hi");
+//     // res.sendStatus(500);
+//     // res.sendStatus(404)// these are known as status code
 
-    // res.json({
-    //     success: true,
-    //     product : [],
-    // })
+//     // res.json({
+//     //     success: true,
+//     //     product : [],
+//     // })
 
-    const pathlocation = path.resolve();
+//     // const pathlocation = path.resolve();
 
-    res.render()
+//     res.render()
+// })
+
+     app.use(express.static(path.join(path.resolve(),"public")));
+     console.log();
+
+
+app.get("/", (req,res) => {
+    // res.render("index.ejs")
+
+    res.render("index.ejs",{name:"Prashant"})
+    // res.sendFile('index')
 })
 
 app.listen(5000, ()=>{
